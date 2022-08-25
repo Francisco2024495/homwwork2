df6ce66f9d5b459d2b357a5e3ff0e76d8b3a05b0
Francisco2024495 <112114284+Francisco2024495@users.noreply.github.com>
2022-08-25 12:46:59 -0500

    Add files via upload

---
+++ hola_soy_franciso.py
@@ -0,0 +1,16 @@
+# Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
+# Es posible que el codigo generado no sea completamente correcto. Si encuentra
+# errores por favor reportelos en el foro (http://pseint.sourceforge.net).
+
+
+if __name__ == '__main__':
+	a = int()
+	b = int()
+	res = int()
+	print("ingresa el primer numero")
+	a = int(input())
+	print("ingresa el segundo numero")
+	b = int(input())
+	res = a+b
+	print("el resultado de la suma de los numeros es:",res)
+
